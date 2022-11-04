4327423fd2a8560a815a4e6b9e62d5c0afd337de
Dan Nelson <danelson@nwi.net>
2022-11-03 17:45:02 -0700

    reformat sql_helper

--- sql_helper.py
+++ sql_helper.py
@@ -1,11 +1,37 @@
-select_all = """SELECT * FROM popular_movies;"""
 
-select_one = """SELECT index, rank, title, year FROM popular_movies 
-        WHERE rank = '3';"""
+insert_one = '''
+INSERT INTO
+    popular_movies(
+        index, rank, title, year
+    )
+VALUES(
+    100, '101', 'XXXX', 'XXX', 
+);
+'''
 
-insert_one = """INSERT INTO popular_movies(index, rank, title, year) VALUES(
-    '100', '101', 'XXXX', 'XXXX');"""
+select_one = """
+SELECT 
+    index, rank, title, year
+FROM
+    popular_movies
+WHERE
+    rank = '3';
+"""
+
+select_all = """
+SELECT
+    *
+FROM
+    popular_movies;
+"""
 
 #customers and orders are lists of tuples with the info for each ?
-add_many = cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?);", customers)
-add_many2 = cur.executemany("INSERT INTO orders VALUES(?, ?, ?, ?);", orders)
+insert_many = """
+INSERT INTO
+    users
+VALUES(
+    ?,?,?,?
+);
+""",
+customers
+
